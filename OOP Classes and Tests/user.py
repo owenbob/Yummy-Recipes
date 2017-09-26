0f31ddcf3fac93239511facc3038233ec639d6e4
owenbob <obyomuhangi@gmail.com>
2017-09-26 15:30:01 +0300

    adding updates to user.py and test_user.py plus initialized recipes.py

--- OOP Classes and Tests/user.py
+++ OOP Classes and Tests/user.py
@@ -1,5 +1,6 @@
 """
 User class that should contain our functions  and variables for testing
+the database list belongs to the app as a databse
 """
 
 class User(object):
@@ -12,3 +13,56 @@ class User(object):
         self.username = username
         self.email = email
         self.password = password
+        self.database = []
+        self.recipes = {}
+        self.new_recipes = {}
+
+    def registerUser(self,list):
+        self.database.append(list)
+        return self.database
+
+    def create_recipe(self, recipe1):
+        #method to check if the list id exists if not,  create a new list
+        
+        if recipe1.recipe_id in self.recipes.keys():
+            return False
+        elif recipe1.title in self.new_recipes:
+            return False
+
+        else:
+            self.recipes[recipe1.recipe_id] = recipe1
+            self.new_recipes[recipe1.recipe_id] = recipe1.title
+            return True
+
+    def get_recipes(self):
+        #method to get all lists that have been created
+        
+        return self.recipes
+
+    def get_recipe(self, recipe_id):
+        #method to get single list using list id
+        
+        if recipe_id in self.recipes.keys():
+            return self.recipes[recipe_id]
+        return None
+    def edit_recipe(self,  title, description,recipe_id):
+      
+        #method to edit the title  and description of an existing recipe 
+        
+        if recipe_id in self.recipes.keys():
+            edited_recipe = self.recipes[recipe_id]
+            edited_recipe.title = title
+            edited_recipe.description = description
+            return True
+        return False
+
+    def del_recipe(self, recipe_id):
+        #method to delete list
+        
+        if recipe_id in self.recipes.keys():
+            del self.recipes[recipe_id]
+            del self.new_recipes[recipe_id]
+            return True
+        return None
+
+
